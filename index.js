5f87bddf329309dd8dbdad76f88f133dd4130998
Student-PIAIC <raoasif452@gmail.com>
2024-04-06 03:13:40 +0500

    created number guessing game

---
+++ index.js
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+//computer will generate a random number
+//user input for guessing number
+//match both numbers and show result gussed number is right or wrong
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessNumber",
+        type: "number",
+        message: "Please guess a number",
+    }
+]);
+if (answer.userGuessNumber == randomNumber) {
+    console.log("Congragulations your guessed number is Right");
+}
+else {
+    console.log("Sorry your guessed number is wrong");
+}
